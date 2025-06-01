c9aa4590c732a1fa0bef8e650ee8e48f6fee36ea
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-06-01 18:54:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5000, // Change this to your desired port
-  },
+server: {
+  host: '0.0.0.0', // Ensure it binds to 0.0.0.0
+  port: 5173, // Use Render's PORT variable or fallback to 5173
+},
 });
